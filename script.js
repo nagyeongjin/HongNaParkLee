ccb19b0e04f6d4c567ea0494c3a76a8e3f34f203
LeeHoJeong <164442057+h0je0ng@users.noreply.github.com>
2024-05-02 12:04:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // 검색 버튼에 대한 이벤트 처리
     let searchBtn = document.querySelector('.search-btn');
     searchBtn.addEventListener('click', function(event) {
-        event.preventDefault(); // 폼 전송 방지
+        event.preventDefault(); 
 
         // 검색어 가져오기
         let searchInput = document.querySelector('.search-txt').value;
 
-        // 선택된 장르 가져오기
         let selectedGenres = [];
         let genreCheckboxes = document.querySelectorAll('.genre-check-box .btn-check');
         genreCheckboxes.forEach(function(checkbox) {
@@ -30,7 +28,6 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log('선택된 장르:', selectedGenres);
         console.log('선택된 연도:', selectedYears);
 
-        // 여기서 검색을 실행하는 로직을 추가하면 됩니다.
-        // 예를 들어, 검색어를 서버로 전송하여 검색 결과를 받아오는 등의 동작을 수행할 수 있습니다.
+        
     });
 });
