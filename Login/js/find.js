d994f5b88dd3628e5dccfdc0837aab3ea97d9430
GyeongJin Na <rudwls2080@gmail.com>
2024-05-06 22:49:39 +0900

    0506 js,css,html

---
+++ Login/js/find.js
@@ -0,0 +1,57 @@
+let update=document.getElementById("update_pw_window")
+let id_window=document.getElementById("id_window")
+
+
+function certiBtnHide(){
+    let certi_btn = document.getElementById("certi_btn");
+    certi_btn.hidden = false;
+}
+
+function findId(){
+
+    let name = document.getElementById("find_name").value;
+    let phone = document.getElementById("find_phone").value;
+    if (!name || !phone) {
+        alert("빈칸 없이 모두 입력해주세요.");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function findPassword() {
+    let id = document.getElementById("find_id").value;
+    let name = document.getElementById("find_name2").value;
+    let phone = document.getElementById("find_phone2").value;
+
+    if (!id || !name || !phone) {
+        alert("빈칸 없이 모두 입력해주세요.");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+function updateOpen(){
+    update.style.display="flex";
+}
+  function updateClose(){
+    update.style.display="none";
+}
+function idOpen(){
+    id_window.style.display="flex";
+}
+function idClose(){
+    id_window.style.display="none";
+}
+document.getElementById("find_id_btn").addEventListener("click", function() {
+    if (findId()) {
+        idOpen();
+    }
+});
+document.getElementById("find_pw_btn").addEventListener("click", function() {
+    if (findPassword()) {
+        updateOpen();
+    }
+});
--- Login/js/register.js
+++ Login/js/register.js
@@ -1,3 +1,21 @@
+let id = document.getElementById("id").value;
+let pw = document.getElementById("pw").value;
+let email=document.getElementById("email").value;
+let pattern_chk1 = /[`~!@#$%^&*|\\\'\";:\/?]/; //아이디 특수문자 체크
+let pattern_chk2 = /[0-9]/;
+let pattern_chk3 = /[a-zA-Z]/;
+let pattern_chk4 = /[~!@#$%^&*()_+|<>?:{}]/;
+let pw_chk = pattern_chk2.test(pw) || !pattern_chk3.test(pw) || !pattern_chk4.test(pw) || pw.length <8 || pw.includes(' ')
+let email_chk = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/;
+let certi_window = document.getElementById("certi_window");
+
+function certiOpen(){
+  certi_window.style.display="flex";
+}
+function certiClose(){
+  certi_window.style.display="none";
+}
+
 $(document).ready(function () {
     setDateBox();
 });
@@ -32,3 +50,27 @@ $(document).ready(function () {
     }
 
 }
+
+function checkReg(){
+    
+	if (id.trim().length == 0) {
+		alert("아이디를 잘못 입력하셨습니다.");
+		return;
+	}	else if(id.length <5 || id.length>10){
+		alert("아이디는 5~10자리 이어야 합니다.");
+	}	else if (pattern_chk1.test(id)) { //아이디 특수문자 체크
+		alert("아이디에 특수문자는 들어갈 수 없습니다.");
+		return;
+	}	else if(pw.trim().length == 0){
+		alert("비밀번호를 입력하시기 바랍니다.");
+		return;
+	} else if (!pw_chk) {
+		alert("비밀번호는 8자리 이상 문자, 숫자, 특수문자로 구성하여야 합니다.");
+		return;
+	}else if(!email_chk.test(email)){
+    alert("이메일형식이 올바르지 않습니다.")
+    return;
+  }
+}
+
+
